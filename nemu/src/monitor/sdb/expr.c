64d720244e2c8f8f530da42976547d9df90b8799
tracer-ics2024 <tracer@njuics.org>
2025-09-04 11:37:55 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:37:55 up  1:44,  2 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -186,7 +186,7 @@ word_t eval(int p,int q,bool *success)
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
-    // Log("%d %d",p+1,q-1);
+     Log("paren %d %d",p+1,q-1);
     return eval(p + 1, q - 1,success);
   }
   else {
