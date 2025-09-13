ee7da8ed459d85fd142bf26c63e41e52ca4ac160
tracer-ics2024 <tracer@njuics.org>
2025-09-13 16:58:29 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:29 up  5:53,  3 users,  load average: 0.05, 0.03, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -32,7 +32,7 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2) {
   int diff = 0;
-  for(int i = 0; i < strlen(s1); ++i)
+  for(int i = 0; ; ++i)
   {
     unsigned char c1 = *s1++;
     unsigned char c2 = *s2++;
