661957aa8deba6aedb30a8a58c00dc0d4c9438ff
tracer-ics2024 <tracer@njuics.org>
2025-09-13 17:30:07 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:30:07 up  6:25,  3 users,  load average: 0.08, 0.03, 0.01

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -63,6 +63,7 @@ void *memcpy(void *out, const void *in, size_t n) {
 
 int memcmp(const void *s1, const void *s2, size_t n) {
   int diff = 0;
+  return 0;
   for(int i = 0; i < n; i++) {
     diff = *(int*)s1++ - *(int*)s2++;
   }
