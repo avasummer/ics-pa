c328c14fffa0cd79b58bc3586191adcc5d1960ee
tracer-ics2024 <tracer@njuics.org>
2025-09-19 15:06:20 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:20 up 16 min,  2 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -23,14 +23,14 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 
 word_t vaddr_read(vaddr_t addr, int len) {
 #ifdef CONFIG_MTRACE
-  printf("v_read %p %n\n", addr, len);
+  printf("p_read 0x" FMT_WORD " %d\n", addr, len);
 #endif
   return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
 #ifdef CONFIG_MTRACE
-  printf("v_wirte %p %n\n", addr, len);
+  printf("p_read 0x" FMT_WORD " %d\n", addr, len);
 #endif
   paddr_write(addr, len, data);
 }
