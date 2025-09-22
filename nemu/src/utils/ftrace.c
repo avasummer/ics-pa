3d8de7304329bf231a064956859e659b5bbb3af8
tracer-ics2024 <tracer@njuics.org>
2025-09-22 19:27:58 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:27:58 up 3 min,  2 users,  load average: 0.10, 0.15, 0.08

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -66,17 +66,6 @@ void init_ftrace(const char* elf) {
   }
   size_t count = fread(&elf_header, 1, sizeof(Elf64_Ehdr), fp);
   assert(count == sizeof(Elf64_Ehdr));
-  long section_header_offset = elf_header.e_shoff;
 
-  Elf64_Shdr section_header;
-  fseek(fp, section_header_offset, SEEK_SET);
-
-  for (int i = 0; i < elf_header.e_shnum; i++) {
-  size_t count = fread(&section_header, 1, sizeof(Elf64_Shdr), fp);
-    assert(count == sizeof(Elf64_Shdr));
-    if (section_header.sh_type == SHT_SYMTAB) {
-      printf("Symbol Table found at offset: 0x%lx, size: %lu\n",
-             (long)section_header.sh_offset, (long)section_header.sh_size);
-    }
-  }
+  parse_symbols(fp, &elf_header);
 }
