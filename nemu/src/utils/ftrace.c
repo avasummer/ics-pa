8a9efc850de4947bdb02bbb6e954804e58c8b093
tracer-ics2024 <tracer@njuics.org>
2025-09-22 19:26:36 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:26:36 up 2 min,  2 users,  load average: 0.14, 0.16, 0.07

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -2,6 +2,61 @@
 #include <elf.h>
 #include <stdio.h>
 
+void init_ftrace(const char *elf);
+void parse_symbols(FILE *fp, Elf64_Ehdr *elf_header) {
+  Elf64_Shdr *sh_table = malloc(elf_header->e_shnum * sizeof(Elf64_Shdr));
+  fseek(fp, elf_header->e_shoff, SEEK_SET);
+  size_t ret = fread(sh_table, sizeof(Elf64_Shdr), elf_header->e_shnum, fp);
+  assert(ret == elf_header->e_shnum);
+
+  Elf64_Shdr *symtab_shdr = NULL;
+  Elf64_Shdr *strtab_shdr = NULL;
+
+  for (int i = 0; i < elf_header->e_shnum; i++) {
+    if (sh_table[i].sh_type == SHT_SYMTAB) {
+      symtab_shdr = &sh_table[i];
+      if (symtab_shdr->sh_link < elf_header->e_shnum) {
+        strtab_shdr = &sh_table[symtab_shdr->sh_link];
+      }
+      break;
+    }
+  }
+
+  if (!symtab_shdr || !strtab_shdr) {
+    printf("Symbol table or string table not found.\n");
+    free(sh_table);
+    return;
+  }
+
+  Elf64_Sym *sym_table = malloc(symtab_shdr->sh_size);
+  fseek(fp, symtab_shdr->sh_offset, SEEK_SET);
+  ret = fread(sym_table, symtab_shdr->sh_size, 1, fp);
+  assert(ret == symtab_shdr->sh_size);
+
+  char *strtab = malloc(strtab_shdr->sh_size);
+  fseek(fp, strtab_shdr->sh_offset, SEEK_SET);
+  ret = fread(strtab, strtab_shdr->sh_size, 1, fp);
+  assert(ret == strtab_shdr->sh_size);
+
+
+  int num_symbols = symtab_shdr->sh_size / sizeof(Elf64_Sym);
+  printf("Parsing %d symbols...\n", num_symbols);
+
+  for (int i = 0; i < num_symbols; i++) {
+    const char *symbol_name = &strtab[sym_table[i].st_name];
+    Elf64_Addr symbol_addr = sym_table[i].st_value;
+    unsigned char symbol_type = ELF64_ST_TYPE(sym_table[i].st_info);
+
+    if (symbol_type == STT_FUNC) {
+      printf("Found function: %s at address 0x%lx\n", symbol_name, symbol_addr);
+    }
+  }
+
+  free(sh_table);
+  free(sym_table);
+  free(strtab);
+}
+
 void init_ftrace(const char* elf) {
   Elf64_Ehdr elf_header;
   FILE *fp = fopen(elf, "rb");
@@ -19,7 +74,7 @@ void init_ftrace(const char* elf) {
   for (int i = 0; i < elf_header.e_shnum; i++) {
   size_t count = fread(&section_header, 1, sizeof(Elf64_Shdr), fp);
     assert(count == sizeof(Elf64_Shdr));
-    if (section_header.sh_type == SHN_COMMON) {
+    if (section_header.sh_type == SHT_SYMTAB) {
       printf("Symbol Table found at offset: 0x%lx, size: %lu\n",
              (long)section_header.sh_offset, (long)section_header.sh_size);
     }
