68e3027e37f5f85045806c612721075f27bc119d
tracer-ics2024 <tracer@njuics.org>
2025-09-23 11:09:54 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:09:54 up 48 min,  2 users,  load average: 0.01, 0.02, 0.00

--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -79,7 +79,9 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   int ret = snprintf(str, size, "%s", insn->mnemonic);
   if (insn->op_str[0] != '\0') {
     ret = snprintf(str + ret, size - ret, "\t%s", insn->op_str);
+    Log("%d",ret);
   }
+    Log("%d",ret);
   FTRACE(insn);
   cs_free_dl(insn, count);
 }
