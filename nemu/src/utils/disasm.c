40a238cef5e536525d9d3adae81c2329c900f11b
tracer-ics2024 <tracer@njuics.org>
2025-09-23 10:33:49 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:33:49 up 12 min,  2 users,  load average: 0.06, 0.04, 0.01

--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -22,7 +22,7 @@ char* ftrace_find(size_t addr);
 #define FTRACE(x) \
   if(!strcmp(x->mnemonic,"jal") || !strcmp(x->mnemonic,"jalr")){ \
   int64_t addr = strtol(x->op_str, NULL, 16); \
-  Log("%ld \n",addr); \
+  Log("%lx \n",addr); \
   char* offset = ftrace_find(pc + addr); \
   if(offset!=NULL) snprintf(str + ret, size - ret, "\t%s", offset); } \
 
