a64e53d6fe79a4671852d831c74880c901550cc6
tracer-ics2024 <tracer@njuics.org>
2025-09-26 10:48:18 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:18 up 49 min,  3 users,  load average: 0.51, 0.78, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,11 +1,13 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
 #include "../include/nemu.h"
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  if(kbd->keycode != AM_KEY_NONE)kbd->keydown = true;
-  kbd->keycode = inw(KBD_ADDR) & ~KEYDOWN_MASK;
+  uint32_t k = inl(KBD_ADDR);
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
