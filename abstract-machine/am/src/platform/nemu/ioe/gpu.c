295fbfba31b3ae2f5aa34a4edd45b56012a2fa8d
tracer-ics2024 <tracer@njuics.org>
2025-09-26 14:49:31 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:31 up  4:51,  4 users,  load average: 0.08, 0.03, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,32 +1,50 @@
 #include <am.h>
 #include <nemu.h>
 #include <stdint.h>
-
+#include <assert.h>
 #include "../include/nemu.h"
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+void get_size(uint32_t* w, uint32_t* h) {
+  uint32_t screen_wh = inl(VGACTL_ADDR);
+  uint32_t screen_w = screen_wh >> 16;
+  uint32_t screen_h = screen_wh & 0xffff;
+  if(screen_w && screen_h)return;
+}
+
 void __am_gpu_init() {
     int i;
-    int w = inl(VGACTL_ADDR) >> 16;  // TODO: get the correct width
-    int h = inl(VGACTL_ADDR) & 0xffff;  // TODO: get the correct height
+    uint32_t w = 0;  // TODO: get the correct width
+    uint32_t h = 0;  // TODO: get the correct height
+    get_size(&w, &h);
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     for (i = 0; i < w * h; i ++) fb[i] = i;
     outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t screen_wh = inl(VGACTL_ADDR);
-  uint32_t screen_w = screen_wh >> 16;
-  uint32_t screen_h = screen_wh & 0xffff;
+  uint32_t w = 0, h = 0;
+  get_size(&w, &h);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = screen_w, .height = screen_h,
+    .width = w, .height = h,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t screen_w=0,screen_h=0;
+  get_size(&screen_w, &screen_h);
+  if(screen_h < y+h || screen_w < x+w)return;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = ctl->pixels;
+  for (int i = y; i < y+h; i++) {
+    for (int j = x; j < x+w; j++) {
+      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
