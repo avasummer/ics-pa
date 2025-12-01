9afd21e8d1123e08eef5eff069fccd70d9621678
tracer-ics2024 <tracer@njuics.org>
2025-12-01 12:08:31 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-32-generic #32~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  2 14:21:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:08:31 up  1:17,  3 users,  load average: 0.00, 0.04, 0.00

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" reg_nameBASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" reg_nameBASIS, WITHOUT WARRANTIES OF
+ * ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #ifndef __ISA_RISCV_H__
 #define __ISA_RISCV_H__
@@ -21,6 +21,20 @@
 typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   vaddr_t pc;
+  word_t mepc;
+  word_t mcause;
+  word_t priv;
+  word_t mtvec;
+  union {
+    struct {
+      uint32_t UIE : 1, SIE : 1, WPRI_0 : 1, MIE : 1;
+      uint32_t UPIE : 1, SPIE : 1, WPRI : 1, MPIE : 1;
+      uint32_t SPP : 1, WPRI_1_2 : 2, MPP : 2, FS : 2;
+      uint32_t XS : 2, MPRV : 1, SUM : 1, MXR : 1;
+      uint32_t TVM : 1, TW : 1, TSR : 1, WPRI_3_10 : 8, SD : 1;
+    } part;
+    word_t val;
+  } mstatus;
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
