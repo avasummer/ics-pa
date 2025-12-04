f74e268cc1e0bf43282507038b5982cc7a9191c2
tracer-ics2024 <tracer@njuics.org>
2025-12-04 22:53:04 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-32-generic #32~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  2 14:21:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:53:04 up  1:02,  3 users,  load average: 0.03, 0.08, 0.15

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <common.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -10,8 +11,11 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)ehdr.e_ident == 0xBADC0DE);
+  
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
