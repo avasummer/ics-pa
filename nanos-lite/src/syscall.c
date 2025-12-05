883ffb1ce1d5694eff1aabaebf5697c21cb048b4
tracer-ics2024 <tracer@njuics.org>
2025-12-05 11:37:37 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:37:37 up 32 min,  2 users,  load average: 0.19, 0.12, 0.08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,10 +5,7 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-  case 1: {
-    yield();
-    return;
-    }
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
