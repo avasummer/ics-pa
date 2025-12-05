471f155a5143732164d072cf29b488d0325ed21b
tracer-ics2024 <tracer@njuics.org>
2025-12-05 16:25:20 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:20 up  5:20,  2 users,  load average: 0.08, 0.05, 0.01

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,6 +18,7 @@
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.mepc = epc;
   cpu.mcause = NO;
+  cpu.mstatus = 0xa00001800;
 
 #ifdef CONFIG_ETRACE
   printf("Interrupted: " FMT_WORD "\n", epc);
