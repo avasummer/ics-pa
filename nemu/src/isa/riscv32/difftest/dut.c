0598a5dc2fc4eba1b8b962fb3fba1d3b5fd19767
tracer-ics2024 <tracer@njuics.org>
2025-12-05 16:14:50 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:50 up  5:09,  2 users,  load average: 0.17, 0.08, 0.04

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -38,6 +38,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     printf("\n [difftest] inequal pc: 0x%lx\n",ref_r->pc);
     ok = false;
   }
+  cpu.mcause=ref_r->mcause;
   CHECKDIFF(mcause);
   CHECKDIFF(mepc);
   CHECKDIFF(mtvec);
