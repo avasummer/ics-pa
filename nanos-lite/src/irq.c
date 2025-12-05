f807af48113d1d7b265ed6f8bb9375dc1ec4f591
tracer-ics2024 <tracer@njuics.org>
2025-12-05 12:07:47 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:07:47 up  1:02,  2 users,  load average: 0.04, 0.04, 0.01

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,9 +2,9 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-  case 1: {
-      yield();
-      return 0;
+  case EVENT_YIELD: {
+    yield();
+    
     }
     default: panic("Unhandled event ID = %d", e.event);
   }
