0513e0a50405bf4013016cfe13734d6a2b0f77c9
tracer-ics2024 <tracer@njuics.org>
2025-12-05 12:01:50 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:01:50 up 56 min,  2 users,  load average: 0.00, 0.00, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,14 +8,20 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 11: ev.event = EVENT_YIELD; break;
-      default: ev.event = EVENT_ERROR; break;
+    case 11: {
+      if (c->GPR1 == -1)
+        ev.event = EVENT_YIELD;
+      else
+        ev.event = EVENT_SYSCALL;
+      break;
     }
-
+    default:
+      ev.event = EVENT_ERROR;
+      break;
+  }
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-
   return c;
 }
 
