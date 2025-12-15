f6b9fa2d54f86385e46ae7c1de55687453622a40
tracer-ics2024 <tracer@njuics.org>
2025-12-15 12:52:08 +0800

    >  compile NEMU 111 222 Linux ubuntu-VMware-Virtual-Platform 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:52:08 up  1:42,  1 user,  load average: 0.05, 0.06, 0.01

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,6 +23,7 @@ void do_syscall(Context *c) {
     return;
   }
   case SYS_write: {
+    LOG_CALL("SYS_write");
     if (c->GPR1 == 1) {
       snprintf("%s", c->GPR3,(const char*)c->GPR2);
     }
